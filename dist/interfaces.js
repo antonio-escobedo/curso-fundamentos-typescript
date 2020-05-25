70b77c9db16b3894719816933c6bd1d8f751c433
statik0 <isc.antonio.escobedo@gmail.com>
2020-05-25 18:26:35 -0500

    Interfaces con parametros opcionales y readonly

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -22,3 +22,23 @@ showPicture({
     orientation: PhotoOrientation.Portrait
     //extra: 'test' //Error
 });
+function generatePicture(config) {
+    var pic = { title: 'Default', date: '2020-05' };
+    if (config.title) {
+        pic.title = config.title;
+    }
+    if (config.date) {
+        pic.date = config.date;
+    }
+    return pic;
+}
+var picture = generatePicture({});
+console.log('Estado Picture: ', picture);
+picture = generatePicture({ title: 'Travel Picture' });
+console.log('Estado Picture: ', picture);
+var user;
+user = { id: 4, username: 'Statik0', isPro: true };
+console.log('user estado:', user);
+user.username = 'Paparazzi';
+// user.id = 1; //Error para compilador
+console.log('user estado:', user);
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -31,3 +31,41 @@ showPicture({
     orientation: PhotoOrientation.Portrait
     //extra: 'test' //Error
 });
+
+interface PictureConfig {
+    title?: string,
+    date?: string,
+    orientation?: PhotoOrientation
+}
+
+function generatePicture(config: PictureConfig) {
+    const pic = {title: 'Default', date: '2020-05'};
+    if(config.title){
+        pic.title = config.title;
+    }
+    if(config.date){
+        pic.date = config.date;
+    }
+
+    return pic;
+}
+
+let picture = generatePicture({});
+console.log('Estado Picture: ', picture);
+
+picture = generatePicture({title: 'Travel Picture'});
+console.log('Estado Picture: ', picture);
+
+//Interface: Usuario
+interface User {
+    readonly id: number;
+    username: string;
+    isPro: boolean;
+}
+
+let user: User;
+user = {id: 4, username: 'Statik0', isPro: true};
+console.log('user estado:' , user);
+user.username = 'Paparazzi';
+// user.id = 1; //Error para compilador
+console.log('user estado:' , user);
